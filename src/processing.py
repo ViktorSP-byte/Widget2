dcf859a9255ab06cc4b577687ad039289ddc463d
Viktor <flystrike1994@gmail.com>
2024-07-01 13:56:18 +0300

    new module

--- src/processing.py
+++ src/processing.py
@@ -6,16 +6,16 @@ data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.51236
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
 
-def filter_by_state(data: list[dict[str, Any]], state_id: str = 'EXECUTED') -> list[dict[str, Any]]:
+def filter_by_state(data: list[dict[str, Any]], state: str = 'EXECUTED') -> list[dict[str, Any]]:
     """Функция фильтрации операций по ключу"""
     new_data = []
     for key in data:
-        if key.get('state') == state_id:
+        if key.get('state') == state:
             new_data.append(key)
     return new_data
 
 
-print(filter_by_state(data))
+print(filter_by_state(data, 'CANCELED'))
 
 
 def sort_by_date(data: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
